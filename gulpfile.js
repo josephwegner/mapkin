e8d529c4d8def766d0f0fdd41af62314b9bcd030
Joe Wegner <joe@bellycard.com>
2014-05-13 09:20:54 -0500

    better building

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp        = require('gulp'),
     include        = require('gulp-include'),
     coffee        = require('gulp-coffee');
+    uglify = require('gulp-uglify');
+    rename = require("gulp-rename");
 
 gulp.task("scripts", function() {
-    gulp.src('src/mapkin.coffee')
+    return gulp.src('src/mapkin.coffee')
         .pipe( include() )
         .pipe( coffee() )
         .pipe( gulp.dest("dist") )
 });
 
-gulp.task('default', ["scripts"]);
+gulp.task('uglify', ['scripts'], function() {
+  gulp.src(['dist/*.js', '!dist/*min.js'])
+    .pipe(uglify())
+    .pipe(rename(function (path) {
+        path.extname = ".min.js"
+     }))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ["scripts", "uglify"]);
